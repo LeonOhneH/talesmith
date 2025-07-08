8af29c643124b5d26e23f13cf4200d1679be8a36
DerEndboss <aaronleipert@protonmail.com>
2025-07-08 10:36:28 +0200

    Javaklassen angelegt für Modellierungsvorbereitung

--- TaleSmith.editor/xtend-gen/TaleSmith/mycommands/CommandHandler.java
+++ TaleSmith.editor/xtend-gen/TaleSmith/mycommands/CommandHandler.java
@@ -7,33 +7,20 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.TreeSelection;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IFileEditorInput;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.xbase.lib.Exceptions;
-import org.eclipse.xtext.xbase.lib.InputOutput;
-import taleSmith.Game;
-import taleSmith.presentation.TaleSmithEditor;
 
 @SuppressWarnings("all")
 public class CommandHandler extends AbstractHandler {
   @Override
   public Object execute(final ExecutionEvent event) throws ExecutionException {
-    InputOutput.<String>println("Executed Custom Command");
-    IWorkbenchPart wbp = HandlerUtil.getActivePart(event);
-    TaleSmithEditor editor = ((TaleSmithEditor) wbp);
-    IEditorInput _editorInput = editor.getEditorInput();
-    IProject project = ((IFileEditorInput) _editorInput).getFile().getProject();
-    ISelection _selection = editor.getSelection();
-    TreeSelection selection = ((TreeSelection) _selection);
-    Object _firstElement = selection.getFirstElement();
-    Game root = ((Game) _firstElement);
-    this.generateFiles(project, this.compile(root));
-    return void.class;
+    throw new Error("Unresolved compilation problems:"
+      + "\nTaleSmithEditor cannot be resolved to a type."
+      + "\nTaleSmithEditor cannot be resolved to a type."
+      + "\nGame cannot be resolved to a type."
+      + "\nGame cannot be resolved to a type."
+      + "\neditorInput cannot be resolved"
+      + "\nselection cannot be resolved"
+      + "\ncompile cannot be resolved");
   }
 
   public void generateFiles(final IProject project, final String content) {
@@ -59,27 +46,8 @@ public class CommandHandler extends AbstractHandler {
     }
   }
 
-  public String compile(final Game game) {
-    StringConcatenation _builder = new StringConcatenation();
-    _builder.append("public class GameMain {");
-    _builder.newLine();
-    _builder.append("    ");
-    _builder.append("public static void main(String[] args) {");
-    _builder.newLine();
-    _builder.append("        ");
-    _builder.append("System.out.println(\"Willkommen im Spiel: ");
-    String _name = game.getName();
-    _builder.append(_name, "        ");
-    _builder.append("\");");
-    _builder.newLineIfNotEmpty();
-    _builder.append("        ");
-    _builder.append("// Hier könntest du durch game.rooms iterieren etc.");
-    _builder.newLine();
-    _builder.append("    ");
-    _builder.append("}");
-    _builder.newLine();
-    _builder.append("}");
-    _builder.newLine();
-    return _builder.toString();
+  public String compile(final /* Game */Object game) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nname cannot be resolved");
   }
 }
